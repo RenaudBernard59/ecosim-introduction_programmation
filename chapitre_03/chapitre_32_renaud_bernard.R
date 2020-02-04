971dd378b7a341004fe6c1eca800f1c2ceaff840
Renaud BERNARD <renaud.bernard@p59production.com>
2020-02-04 21:15:24 +0100

    Chapitre 3bis END
    I have make all 15 questions of this exercice

--- chapitre_03/chapitre_32_renaud_bernard.R
+++ chapitre_03/chapitre_32_renaud_bernard.R
@@ -4,50 +4,58 @@
 #Rendu Exerice Chapitre 3bis - pour le 05/02/2020
 #Renaud BERNARD - R version 3.6.1 (2019-07-05)
 ############################################################
+options(encoding ="UTF-8") #Je préfère définir de manière globale l'encodage pour ne plus avoir à y penser
+maTable <- read.csv2("13_table_exercice.csv", stringsAsFactors = FALSE) #Que j'utilise par habitude & Fainéantise
+head(maTable)
 
 ### Q1 - Quelle est la dimension du tableau ?
-
+dim(maTable)
 
 ### Q2 - Quel est le nom des variables du tableau ?
-
+colnames(maTable)
 
 ### Q3 - Changer le nom de la huitième colonne en "classement".
-
+colnames(maTable)[8] <- "classement"
+colnames(maTable) #Je vérifie que le changement est bien effectif
 
 ### Q4 - Quel est le format des données du tableau ? 
-
+str(maTable)
 
 ### Q5 - Quel est le nom du film classé 1er ?
-
+maTable[maTable$classement==1,]$titre
 
 ### Q6 - Combien d'entrées a fait le film "Tout est possible" ?
-
+maTable[maTable$titre=="Tout est possible",]$entrees
 
 ### Q7 - Combien de films ont fait plus d'un million d'entrées ?
-
+nrow(maTable[maTable$entrees > 1000000,]) 
 
 ### Q8 - Combien de films sont des reprises ? Quels sont leurs titres ?
-
+estReprise <- maTable[maTable$reprise == TRUE,] #J'enregistre le résultant dans une variable
+#Nombre de reprise
+nrow(estReprise)
+#Leurs noms
+estReprise$titre
 
 ### Q9 - Quel est le genre du film "Made in China" ?
-
+maTable[maTable$titre == "Made in China",]$genre
 
 ### Q10 - Combien d'entrées a réalisé le film "Pokémon Détective Pikachu" ?
-
+maTable[maTable$titre == "Pokémon Détective Pikachu",]$entrees
 
 ### Q11 - Combien de films ont fait moins de 30000 entrées ?
-
+nrow(maTable[maTable$entrees > 30000,])
 
 ### Q12 - Combien de films ne sont pas des films français ?
-
+nrow(maTable[maTable$pays != "France",])
 
 ### Q13 - Combien de documentaires ?
-
+nrow(maTable[maTable$genre != "Documentaires",])
 
 ### Q14 - Quel est le titre original du film "Les Enfants de la mer" ?
-
+maTable[maTable$titre == "Les Enfants de la mer",]$titre_original
 
 ### Q15- Quelle est la nationalité du film classé 100ème ?
+maTable[maTable$classement == 100,]$pays
 
-
-
+##FIN
