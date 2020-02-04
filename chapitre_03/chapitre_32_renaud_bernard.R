4cf35d06f6fb0d3ab1543f96d108944c0fb8e401
Renaud BERNARD <renaud.bernard@p59production.com>
2020-02-04 20:25:55 +0100

    Chapitre 3bis template
    Addind of CSV data file for exercice 3bis and templating R file with metadate header and ALL 15 questions.

---
+++ chapitre_03/chapitre_32_renaud_bernard.R
@@ -0,0 +1,53 @@
+############################################################
+#MASTER e-COSIM
+#Cours Initiation Programmation - Christophe Cariou
+#Rendu Exerice Chapitre 3bis - pour le 05/02/2020
+#Renaud BERNARD - R version 3.6.1 (2019-07-05)
+############################################################
+
+### Q1 - Quelle est la dimension du tableau ?
+
+
+### Q2 - Quel est le nom des variables du tableau ?
+
+
+### Q3 - Changer le nom de la huitième colonne en "classement".
+
+
+### Q4 - Quel est le format des données du tableau ? 
+
+
+### Q5 - Quel est le nom du film classé 1er ?
+
+
+### Q6 - Combien d'entrées a fait le film "Tout est possible" ?
+
+
+### Q7 - Combien de films ont fait plus d'un million d'entrées ?
+
+
+### Q8 - Combien de films sont des reprises ? Quels sont leurs titres ?
+
+
+### Q9 - Quel est le genre du film "Made in China" ?
+
+
+### Q10 - Combien d'entrées a réalisé le film "Pokémon Détective Pikachu" ?
+
+
+### Q11 - Combien de films ont fait moins de 30000 entrées ?
+
+
+### Q12 - Combien de films ne sont pas des films français ?
+
+
+### Q13 - Combien de documentaires ?
+
+
+### Q14 - Quel est le titre original du film "Les Enfants de la mer" ?
+
+
+### Q15- Quelle est la nationalité du film classé 100ème ?
+
+
+
