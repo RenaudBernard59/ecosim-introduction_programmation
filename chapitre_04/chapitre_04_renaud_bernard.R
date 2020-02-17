fa06c30162ab0c414963668c48627dcd441d9084
Renaud BERNARD <renaud.bernard@p59production.com>
2020-02-17 15:38:14 +0100

    Chapitre04 init
    Initialisation Chapitre04

---
+++ chapitre_04/chapitre_04_renaud_bernard.R
@@ -0,0 +1,27 @@
+############################################################
+#MASTER e-COSIM
+#Cours Initiation Programmation - Christophe Cariou
+#Rendu Exerice Chapitre 4 - pour le 21/02/2020
+#Renaud BERNARD - R version 3.6.1 (2019-07-05)
+############################################################
+
+### Q1 - Créer un tableau avec les lignes paires uniquement ?
+
+### Q2 - Combien de films d'animation japonaise ?
+
+### Q3 - Combien de films chinois ou indiens ?
+
+### Q4 - Combien de films sont des comédies ?
+
+### Q5 - Quel est le pourcentage de films dont le titre français est le titre original ?
+
+### Q6 - Quel est le titre et le titre original du film suivant : le film est une reprise et le titre original diffère du nom français du film ?
+
+### Q7 - Quel est le nombre d'entrées du plus gros documentaire français ?
+# Indications : créer une table, ordonner la table, focaliser sur la première ligne.
+
+### Q8 - Combien de films font plus d'entrées que ce documentaire ?
+
+### Q9 - Combien de films non documentaires font moins d'entrées que ce documentaire ?
+
+### Q10 - Créer un tableau de 10 lignes prises au hasard entre le rang 100 et 200 !
